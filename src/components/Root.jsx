a395a55cebe93d0dd7b3ea360fe65661dfe32dd9
Khalim Khamzin <twoxowt@yandex.ru>
2023-04-03 19:40:51 +0300

    refactoring

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch,Redirect} from "react-router-dom";
 import {App} from "../App";
 import {AnimePage} from "../pages/AnimePage";
 import {Navbar} from "./Navbar";
@@ -13,13 +13,17 @@ export const Root = ()=>{
             <Navbar/>
             <Switch>
                 <Route exact={true} path="/anime" component={App}/>
+                <Route path="/animeSite" component={App}/>
                 <Route  path="/anime/:mal_id" component={AnimePage}/>
                 <Route  path="/search" component={SearchPage}/>
                 <Route  path="/favorite" component={FavoritePage}/>
                 <Route path="/search/:id_genre" component={SearchPage}/>
+                <Route path="/search/:id_genre" component={SearchPage}/>
                 <Route >
                    <h1>ERROR</h1>
                 </Route>
+
+
             </Switch>
         </BrowserRouter>
     )
